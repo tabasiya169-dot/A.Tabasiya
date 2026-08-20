c77a22e338e5fa9b0e8e8885c189b65f6641ff5c
Tabasiya <tabasiya169@gmail.com>
2026-08-20 20:39:41 +0530

    JS Day 4

---
+++ JS/Day4/Task2/assets/js/index.js
@@ -0,0 +1,7 @@
+let number = 15;
+
+if (number % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
---
+++ JS/Day4/Task3/assets/js/index.js
@@ -0,0 +1,13 @@
+let mark = 85;
+
+if (mark >= 90 && mark <= 100) {
+    console.log("A+");
+} else if (mark >= 75 && mark <= 89) {
+    console.log("A");
+} else if (mark >= 50 && mark <= 74) {
+    console.log("B");
+} else if (mark >= 35 && mark <= 49) {
+    console.log("C");
+} else {
+    console.log("Fail");
+}
